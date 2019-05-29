6d2287d20e1ae2d465548d828e8a53616ce4a04d
WestonElliott <Westonini101@gmail.com>
2019-05-29 02:08:47 -0700

    - Added music w/ a txt file that shows the source - Added button click sounds to PLAY and MAIN MENU - Edited SupportMain's sprite slightly - Some other stuff

--- Summer2019Game1/Assets/Scripts/MainMenu.cs
+++ Summer2019Game1/Assets/Scripts/MainMenu.cs
@@ -5,18 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("Music");
+    }
+
     public void PlayGame() //Goes to next scene
     {
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame() //Quits game
     {
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
         Application.Quit();
     }
 
     public void MainMenuButton() //Goes back one scene 
     {
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
